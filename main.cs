967dc157d8090edff9d6ae19fae5bf05602db381
ADenzie <158657823+ADenzie@users.noreply.github.com>
2024-02-17 02:39:53 +0000

    Added that the next input will be added to the first input(Final Modification)

--- main.cs
+++ main.cs
@@ -5,11 +5,13 @@ class Program
     static void Main()
     {
         string userInput = "";
+        string totalInput = "";
         Console.WriteLine("Enter text (type 'exit' to end program):");
 
         while ((userInput = Console.ReadLine()) != "exit")
         {
-            Console.WriteLine("You entered: " + userInput);
+            totalInput += userInput + " ";
+            Console.WriteLine("You entered: " + totalInput);
         }
         Console.WriteLine("Program terminated.");
     }
