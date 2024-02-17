01fde2169d6abaf9b2c866a44bb92a14cc8c16d5
ADenzie <158657823+ADenzie@users.noreply.github.com>
2024-02-17 01:55:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string userInput = "";
+        Console.WriteLine("Enter text (type 'exit' to end program):");
+
+        while ((userInput = Console.ReadLine()) != "exit")
+        {
+            Console.WriteLine("You entered: " + userInput);
+        }
+
+        Console.WriteLine("Program terminated.");
+    }
+}
